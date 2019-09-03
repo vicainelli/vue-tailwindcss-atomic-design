9c2f242224fb89ef68edc0e51fa3e6893fd73bca
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-09-02 23:40:03 -0300

    Create Home page component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <page-home></page-home>
+  </div>
 </template>
 
 <script>
+import Home from '@/components/pages/Home/Home'
+
 export default {
-  name: "app"
-};
+  name: 'app',
+  components: {
+    'page-home': Home
+  }
+}
 </script>
