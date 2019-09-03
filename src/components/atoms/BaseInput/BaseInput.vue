e3805087fb4d7233c2693e32ee359e15df783020
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-09-03 00:04:56 -0300

    Create BaseInput component

---
+++ src/components/atoms/BaseInput/BaseInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <label class="uppercase text-sm font-semibold text-gray-700">
+    {{ label }}
+    <input type="text" class="form-input mt-1 block w-full text-gray-600" />
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'base-input',
+  props: {
+    // Label
+    label: {
+      type: String,
+      default: undefined
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
